b2fbcbcfde7d77680ce1584f43995d5b10e1c08d
VINICYUSDABANANA <s.vinicyus@escola.pr.gov.br>
2025-05-06 12:33:42 -0300

    capitulo 2 expressoes e instruçoes

---
+++ pythonProject2/Capitulo 2/exemplo2nomesvariaveis.py
@@ -0,0 +1,7 @@
+# Variaveis nao podem começar com numeros
+# 76trombones = 'big parede'
+
+# Variaveis nao podem começar come letras maiusculas
+# More = 1000000
+# Variaveis nao podem ter caracteres especiais
+# tirando o underline
