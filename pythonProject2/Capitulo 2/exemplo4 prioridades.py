4c66ce04bdfedde22211d853e9698f1365950c4c
kay <kaylaine.pedro@escola.pr.gov.br>
2025-05-13 11:36:34 -0300

    capitulo1

---
+++ pythonProject2/Capitulo 2/exemplo4 prioridades.py
@@ -0,0 +1,15 @@
+#operador de soma
+soma = 2 + 1
+
+#operador de subtracao
+subtracao = 2 - 1
+
+#operador de multiplicacao
+multiplicacao = 2 * 1
+
+#operador de divisão
+divisao = 2 / 1
+
+#operador de potencia
+potencia = 2 ** 1
+
