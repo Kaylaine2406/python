802ec206ce334a4e0a962a81039fb0e3d87292e6
Chris <christian.linarez@escola.gov.br>
2025-05-20 11:44:42 -0300

    exemplo 2 Valor e tipo

---
+++ pythonProject2/capitulo 1/exemplo2valorestipos.py
@@ -0,0 +1,17 @@
+# Tipo inteiro 1,2,3,..-2,-3
+
+inteiro =  2
+print(type(inteiro))
+
+#tipo flutualnte  float 3.3, 0.6 ... -3.456
+float = 1.3
+print(type(float))
+
+#caracteres
+char1 = 'a'
+char2 = '2'
+print(type(char1))
+
+#string conjunto de caracteres
+stringNome = "Kaylaine Mariana"
+print(type(stringNome))
